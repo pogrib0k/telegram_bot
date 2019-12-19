dde304237cee3935e2adffb07555cde860341cb7
Denis Pogrebnyak <pogrib7@gmail.com>
2019-12-20 00:15:22 +0200

    Forward

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/pogrib0k/telegram_bot
 
 go 1.13
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/tools v0.0.0-20191219212307-145a1e401f50 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
+	bot, err := tgbotapi.NewBotAPI("934013449:AAGU3PstPF0F5_HWIFkg3OWi1Ao4uKGguzY")
+	if err != nil {
+		log.Panic(err)
+	}
 
-	fmt.Println("У меня тепрь ГО головного мозха! ((((")
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if update.Message.Sticker != nil {
+			fwd := tgbotapi.NewForward(update.Message.Chat.ID, update.Message.Chat.ID, update.Message.MessageID)
+			bot.Send(fwd)
+		}
+
+		bot.Send(msg)
+	}
 }
