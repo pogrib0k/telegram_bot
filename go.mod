ed01f1f6452e2de8d6c8d54f11204ddd559012a1
Denis Pogrebnyak <pogrib7@gmail.com>
2019-12-23 21:58:37 +0200

    Stickers

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/tools v0.0.0-20191219212307-145a1e401f50 // indirect
+	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
 )
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"image/png"
 	"log"
+	"net/http"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/image/webp"
 )
+
+// Check делает check, ну шо го ты доволен?! А?!
+func Check(path string) bool {
+	if len(path) == 0 {
+		return false
+	}
+	return strings.HasSuffix(path, ".webp")
+}
+
+func CheckText(str string) int {
+
+	str = strings.ToLower(str)
+	if strings.Contains(str, "ping") {
+		return 1
+	}
+	return 0
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI("934013449:AAGU3PstPF0F5_HWIFkg3OWi1Ao4uKGguzY")
@@ -12,7 +34,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -29,11 +51,63 @@ func main() {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		if CheckText(update.Message.Text) == 1 {
+
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "pong!")
+			bot.Send(msg)
+			continue
+		}
+
 		msg.ReplyToMessageID = update.Message.MessageID
 
 		if update.Message.Sticker != nil {
-			fwd := tgbotapi.NewForward(update.Message.Chat.ID, update.Message.Chat.ID, update.Message.MessageID)
-			bot.Send(fwd)
+			//fwd := tgbotapi.NewForward(update.Message.Chat.ID, update.Message.Chat.ID, update.Message.MessageID)
+
+			FileID := update.Message.Sticker.FileID
+
+			//log.Println(FileID)
+
+			var fc tgbotapi.FileConfig
+
+			fc.FileID = update.Message.Sticker.FileID
+
+			pathURL, err := bot.GetFileDirectURL(FileID)
+			log.Println(pathURL)
+			resp, err := http.Get(pathURL)
+
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			if !Check(pathURL) {
+				msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Ты ебанат, порядок не тот!!!")
+				bot.Send(msg)
+				continue
+			}
+			defer resp.Body.Close()
+
+			img0, err := webp.Decode(resp.Body)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			bufPNG := new(bytes.Buffer)
+			err = png.Encode(bufPNG, img0)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			photoUpload := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, tgbotapi.FileBytes{Name: "ban.png", Bytes: bufPNG.Bytes()})
+
+			_, err = bot.Send(photoUpload)
+
+			if err != nil {
+				log.Println(err.Error())
+			}
+
 		}
 
 		bot.Send(msg)
